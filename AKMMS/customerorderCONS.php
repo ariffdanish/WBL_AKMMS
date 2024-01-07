98d15f069dc6f1bdac6cc09461ff7a84b8482599
MuhammadImanFirdaus <muhammadimanfirdaus@graduate.utm.my>
2024-01-08 00:15:41 +0800

    Update customerorderCONS.php

--- AKMMS/customerorderCONS.php
+++ AKMMS/customerorderCONS.php
@@ -48,13 +48,8 @@ $result = mysqli_query($con, $sql);
                             echo "<td>" . $row['c_name'] . "</td>";
                             echo "<td>" . $row['Ord_name'] . "</td>";
                             echo "<td>" . $row['Ord_date'] . "</td>"; 
-<<<<<<< Updated upstream
-                            echo "<td>" . $row['Ord_date'] . "</td>"; 
-                            echo "<td style='text-align: center;'>";
-=======
 
                             echo "<td style='text-align: initial;'>";
->>>>>>> Stashed changes
                             echo "<a href='customercancelCONS.php?id=" . $row['Ord_id'] . "' class='btn btn-danger mr-2' onclick='return confirmDelete()'><i class='fas fa-times'></i> </a>&nbsp ";
                             echo "<a href='customereditCONS.php?id=" . $row['Ord_id'] . "' class='btn btn-primary mr-2'><i class='fas fa-edit'></i> </a>";
                             
