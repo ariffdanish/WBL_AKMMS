718ebb9c19df782ba93a2d1afe1b35ad646c607a
MuhammadImanFirdaus <muhammadimanfirdaus@graduate.utm.my>
2024-01-14 01:45:45 +0800

    s

--- AKMMS/Quotation.php
+++ AKMMS/Quotation.php
@@ -185,10 +185,11 @@ $ordId = isset($_GET['Ord_cid']) ? intval($_GET['Ord_cid']) : 0;
             
 
             // Use the existing connection from your connection file
-            $queryItems = "SELECT q.q_itemDesc, q.q_quantity, q.q_price,q.q_discount,q.q_tax 
-               FROM tb_quotation q
-               JOIN tb_order o ON q.q_ordID = o.Ord_id
-               WHERE o.Ord_cid = $ordId";
+            $queryItems = "SELECT q.q_itemDesc, q.q_quantity, q.q_price, q.q_discount, q.q_tax 
+            FROM tb_quotation q
+            JOIN tb_order o ON q.q_ordID = o.Ord_id
+            WHERE o.Ord_cid = $ordId AND q.q_totalcost <> 0";
+
 
             $resultItems = mysqli_query($con, $queryItems);
 
