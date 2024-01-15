fb4dbc15de8ea33702eee05db7ffaf4281b51b51
ariffdanish <muhammadariffdanish@graduate.utm.my>
2024-01-15 16:55:23 +0800

    siuuu

--- AKMMS/customerdetailsformprocess.php
+++ AKMMS/customerdetailsformprocess.php
@@ -1,7 +1,6 @@
 <?php
 include('mysession.php');
-if (!session_id()) 
-{
+if (!session_id()) {
     session_start();
 }
 include('dbconnect.php');
@@ -14,45 +13,56 @@ $caddress = $_POST['caddress'];
 $cemail = $_POST['cemail'];
 $ctype = $_POST['ctype'];
 
-// Insert New Customer
-$sql = "INSERT INTO tb_customer(c_idnum, c_name, c_phone, c_address, c_email, c_type)
-        VALUES('$cidnum', '$cname', '$cphone', '$caddress', '$cemail', '$ctype')";
+// Check if customer ID already exists
+$sqlCheck = "SELECT COUNT(*) as count FROM tb_customer WHERE c_idnum = '$cidnum'";
+$resultCheck = mysqli_query($con, $sqlCheck);
+$rowCheck = mysqli_fetch_assoc($resultCheck);
 
-mysqli_query($con,$sql);
-mysqli_close($con);
+if ($rowCheck['count'] > 0) {
+    // If customer ID already exists, display an error message or redirect to a page
+    echo "Customer ID already exists. Please insert a different ID.";
+} else {
+    // Insert New Customer
+    $sql = "INSERT INTO tb_customer(c_idnum, c_name, c_phone, c_address, c_email, c_type)
+            VALUES('$cidnum', '$cname', '$cphone', '$caddress', '$cemail', '$ctype')";
 
-// Display Result
-include 'headerNav.php';
+    mysqli_query($con, $sql);
+    mysqli_close($con);
+
+    // Display Result
+    include 'headerNav.php';
+    ?>
+
+    <div class="container" style="margin-top: 20px; padding: 20px;">
+        <table class="table">
+            <tr>
+                <td><strong>Customer ID:</strong></td>
+                <td><?php echo $cidnum; ?></td>
+            </tr>
+            <tr>
+                <td><strong>Name:</strong></td>
+                <td><?php echo $cname; ?></td>
+            </tr>
+            <tr>
+                <td><strong>Tel. No:</strong></td>
+                <td><?php echo $cphone; ?></td>
+            </tr>
+            <tr>
+                <td><strong>Address:</strong></td>
+                <td><?php echo $caddress; ?></td>
+            </tr>
+            <tr>
+                <td><strong>Email:</strong></td>
+                <td><?php echo $cemail; ?></td>
+            </tr>
+            <tr>
+                <td><strong>Type:</strong></td>
+                <td><?php echo $ctype; ?></td>
+            </tr>
+        </table>
+        <a class="btn btn-danger" href="customerdetails.php">Back</a>
+    </div>
+
+    <?php include 'footer.php';
+}
 ?>
-
-<div class="container" style="margin-top: 20px; padding: 20px;">
-    <table class="table">
-        <tr>
-            <td><strong>Customer ID:</strong></td>
-            <td><?php echo $cidnum; ?></td>
-        </tr>
-        <tr>
-            <td><strong>Name:</strong></td>
-            <td><?php echo $cname; ?></td>
-        </tr>
-        <tr>
-            <td><strong>Tel. No:</strong></td>
-            <td><?php echo $cphone; ?></td>
-        </tr>
-        <tr>
-            <td><strong>Address:</strong></td>
-            <td><?php echo $caddress; ?></td>
-        </tr>
-        <tr>
-            <td><strong>Email:</strong></td>
-            <td><?php echo $cemail; ?></td>
-        </tr>
-        <tr>
-            <td><strong>Type:</strong></td>
-            <td><?php echo $ctype; ?></td>
-        </tr>
-    </table>
-    <a class="btn btn-danger" href="customerdetails.php">Back</a>
-</div>
-
-<?php include 'footer.php'; ?>
--- AKMMS/customerorderformCONSprocess.php
+++ AKMMS/customerorderformCONSprocess.php
@@ -1,51 +1,63 @@
 <?php
-include ('mysession.php');
-if(!session_id())
-{
+include('mysession.php');
+if (!session_id()) {
     session_start();
 }
-    include 'headerNav.php';
-    include('dbconnect.php');
+include('headerNav.php');
+include('dbconnect.php');
 
-    // Order Information
-    $Ord_name = $_POST['Ord_name'];
-    $Ord_cid = $_POST['Ord_cid'];
-    $Ord_date = $_POST['Ord_date'];
-    $Ord_type = $_POST['Ord_type'];
+// Order Information
+$Ord_name = $_POST['Ord_name'];
+$Ord_cid = $_POST['Ord_cid'];
+$Ord_date = $_POST['Ord_date'];
+$Ord_type = $_POST['Ord_type'];
 
+// Check if order name already exists for the specified customer
+$sqlCheck = "SELECT COUNT(*) as count FROM tb_order WHERE Ord_name = '$Ord_name' AND Ord_cid = '$Ord_cid'";
+$resultCheck = mysqli_query($con, $sqlCheck);
+$rowCheck = mysqli_fetch_assoc($resultCheck);
 
+if ($rowCheck['count'] > 0) {
+    // If order name already exists for the specified customer, display an alert and a back button
+    ?>
 
-// Insert into tb_order using the obtained c_idnum
-$sql = "INSERT INTO tb_order (Ord_name, Ord_cid, Ord_date, Ord_type) 
-        VALUES ('$Ord_name', '$Ord_cid', '$Ord_date', '$Ord_type')";
+    <div class="container" style="margin-top: 20px; padding: 20px; text-align: center;">
+        <div class="alert alert-danger" role="alert">
+            Order name already exists for this customer ! 
+        </div>
+        <a class="btn btn-danger" href="customerorderformCONS.php">Back</a>
+    </div>
 
-mysqli_query($con, $sql);
+    <?php
+} else {
+    // Insert into tb_order using the obtained c_idnum
+    $sql = "INSERT INTO tb_order (Ord_name, Ord_cid, Ord_date, Ord_type) 
+            VALUES ('$Ord_name', '$Ord_cid', '$Ord_date', '$Ord_type')";
+
+    mysqli_query($con, $sql);
 
     // Close the database connection
     mysqli_close($con);
+    ?>
+
+    <div class="container" style="margin-top: 20px; padding: 20px;">
+        <table class="table">
+            <tr>
+                <td><strong>Order Name :</strong></td>
+                <td><?php echo $Ord_name; ?></td>
+            </tr>
+            <tr>
+                <td><strong>Date :</strong></td>
+                <td><?php echo $Ord_date; ?></td>
+            </tr>
+            <tr>
+                <td><strong>Order Type:</strong></td>
+                <td><?php echo $Ord_type; ?></td>
+            </tr>
+        </table>
+        <a class="btn btn-danger" href="customerorderCONS.php">Back</a>
+    </div>
+
+    <?php include 'footer.php';
+}
 ?>
-
-
-
-<div class="container" style="margin-top: 20px; padding: 20px;">
-    <table class="table">
-        <tr>
-            <td><strong>Order Name :</strong></td>
-            <td><?php echo $Ord_name; ?></td>
-        </tr>
-        <tr>
-            <td><strong>Date :</strong></td>
-            <td><?php echo $Ord_date; ?></td>
-        </tr>
-        <tr>
-            <td><strong>Order Type:</strong></td>
-            <td><?php echo $Ord_type; ?></td>
-        </tr>
-
-    </table>
-    <a class="btn btn-danger" href="customerorderCONS.php">Back</a>
-</div>
-
-
-
-<?php include 'footer.php'; ?>
